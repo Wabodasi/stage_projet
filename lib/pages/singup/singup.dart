0c08687c1d71dfac53a5c20524d28a18a4769bc9
Wabo Mbobda <wabodasi@gmail.com>
2024-11-21 08:25:44 +0100

    modification sur la page singup

--- lib/pages/singup/singup.dart
+++ lib/pages/singup/singup.dart
@@ -1,185 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:stage_projet/pages/singup/completeYourSingup.dart';
 
-class Singup extends StatelessWidget{
+class Singup extends StatelessWidget {
   const Singup({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: getAppBar(context),
       body: Container(
-        padding: const EdgeInsets.all(12),
-        width: double.infinity,
-        // Creation du degrade
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xffd0dffa), Color(0xffffffff)],
+          padding: const EdgeInsets.only(
+            top: 11,
+            right: 20,
+            bottom: 11,
+            left: 20,
+          ),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Color(0xffd4e2f9), Color(0xffffffff)],
             stops: [0.01, 0.25],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-          )
-        ),
-
-        child: Column(
-          children: [
-            SizedBox(
-              child: Container(
-                alignment: Alignment.topLeft,
-                height: 60,
-                margin: const EdgeInsets.fromLTRB(0, 12, 0, 0),
-                //leftcolor: Colors.red,
-                child: const Text("Sign up",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20
+          )),
+          child: SafeArea(
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 5,),
+                  // Creation bouton retour
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.black54)),
+                    child: BackButton(onPressed: () {}),
                   ),
-                ),
+
+                ],
               ),
             ),
-            const SizedBox(height: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("login with phone number",
-                style: TextStyle(
-                  color: Colors.black
-                ),),
-
-                const SizedBox(height: 4),
-
-                Container(
-                  //color: Colors.lightGreen,
-                  width: double.infinity,
-                  height: 60,
-                  padding: const EdgeInsets.fromLTRB(2, 5, 10, 5),
-
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(17.0)
-                        )
-                      ),
-                      icon: Container(
-                        width: 70,
-                        height: double.infinity,
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(6),
-                        decoration: BoxDecoration(
-                          color: const Color(0xffd0dffa),
-                          borderRadius: BorderRadius.circular(17.0)
-                        ),
-                        child: const Text('🇨🇲+237'),
-                      )
-                    ),
-                  ),
-                ),
-
-                Container(
-                  padding: const EdgeInsets.fromLTRB(2, 0, 0, 0),
-                  child: const Text("A code  will be sent to your number",
-                    style: TextStyle(
-                        color: Colors.black45,
-                        fontSize: 12
-                    ),
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 35,
-                ),
-
-                Container(
-                  //color: Colors.orange,
-                  width: double.infinity,
-                  height: 60,
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  alignment: Alignment.center,
-
-                  // Creation du bouton continuer
-                  child: TextButton(
-                    onPressed: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return getModalBottomSheet(context);
-                          });
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue
-                    ),
-                    child: Container(
-                      width: double.infinity,
-                      height: 30,
-                      alignment: Alignment.center,
-                      child: const Text("Continue",),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 5,
-                ),
-
-                // texte de fin
-                Container(
-                  width: double.infinity,
-                  height: 100,
-                  //alignment: Alignment.center,
-                  //color: Colors.amberAccent,
-                  padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                  child: const Text('By signing up you agree to the. terms of service and'
-                      'Privacy Policy including Cookie Use',
-                    textAlign: TextAlign.center,
-                    style:TextStyle(
-                      fontSize: 11,
-                      color: Colors.black45,
-                    ),
-                  ),
-                )
-              ]
-
-            )
-
-          ],
-        ),
-      )
-
+          )),
     );
-
   }
 
-  AppBar getAppBar(BuildContext context){
+  AppBar getAppBar(BuildContext context) {
     return AppBar(
       leading: Container(
         margin: const EdgeInsetsDirectional.fromSTEB(12, 12, 0, 0),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
-            border: Border.all(
-                color: Colors.black54
-            )
-        ),
-        child: BackButton(
-          onPressed: () {}
-
-        ),
+            border: Border.all(color: Colors.black54)),
+        child: BackButton(onPressed: () {}),
       ),
       backgroundColor: const Color(0xffd0dffa),
     );
   }
 
-  Widget getModalBottomSheet(BuildContext context){
+  Widget getModalBottomSheet(BuildContext context) {
     return Container(
       height: 500,
-
       decoration: const BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.horizontal(
-              left: Radius.circular(29),
-              right: Radius.circular(29)
-          )
-      ),
+              left: Radius.circular(29), right: Radius.circular(29))),
       padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -187,37 +76,32 @@ class Singup extends StatelessWidget{
           Container(
             width: 250,
             margin: const EdgeInsets.fromLTRB(0, 60, 0, 30),
-            child: const Text('Is you phone number correct ?',
-              style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20
-              ),
+            child: const Text(
+              'Is you phone number correct ?',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
             ),
           ),
           const Text('(+237)690 000 000',
               style: TextStyle(
                 fontWeight: FontWeight.w700,
-              )
-          ),
+              )),
           Container(
             margin: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-            child: const Text('Med Prepa will send a verification code to you phone number.'),
+            child: const Text(
+                'Med Prepa will send a verification code to you phone number.'),
           ),
           TextButton(
             onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: ((context) => CompleteYourSingup())
-                )
-              );
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: ((context) => CompleteYourSingup())));
             },
-            style: TextButton.styleFrom(
-                backgroundColor: Colors.blue
-            ),
+            style: TextButton.styleFrom(backgroundColor: Colors.blue),
             child: Container(
               height: 30,
               alignment: Alignment.center,
-              child: const Text("Confirm",),
+              child: const Text(
+                "Confirm",
+              ),
             ),
           )
         ],
@@ -225,6 +109,3 @@ class Singup extends StatelessWidget{
     );
   }
 }
-
-
-
